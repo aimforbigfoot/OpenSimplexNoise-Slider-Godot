b6d78f8beec6776f280fc5a73b99d198cd117ed2
aimforbigfoot <61890796+aimforbigfoot@users.noreply.github.com>
2020-12-06 20:27:18 -0500

    Inital Commit

---
+++ OpenSimplexNoiseSlider/src/GameScene.gd
@@ -0,0 +1,45 @@
+extends Node2D
+
+onready var tilemap := $TileMap
+export var width := 320
+export var height := 151
+export var in_seed := ""
+var osn := OpenSimplexNoise.new()
+
+
+
+func _ready() -> void:
+	randomize()
+	if in_seed:
+		osn.seed = hash(in_seed)
+		print(hash(in_seed))
+	else:
+		osn.seed = randi()
+	generate_map()
+
+func generate_map() -> void:
+	for x in width:
+		for y in height:
+			var rand := floor((abs(osn.get_noise_2d(x,y)))*15)
+			tilemap.set_cell(x,y, rand)
+	pass
+
+
+func _on_period_slider_value_changed(value: float) -> void:
+	osn.period = value
+	generate_map()
+
+
+func _on_octave_slider_value_changed(value: float) -> void:
+	osn.octaves = value
+	generate_map()
+
+
+func _on_persist_slider_value_changed(value: float) -> void:
+	osn.persistence = value
+	generate_map()
+
+
+func _on_lacun_slider_value_changed(value: float) -> void:
+	osn.lacunarity = value
+	generate_map()
